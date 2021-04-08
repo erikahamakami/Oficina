b033ab4f2ea5cf392e7d77e92b47793ab5092113
Luís <luismoita1337@gmail.com>
2021-04-08 20:57:09 -0300

    update do backend

---
+++ front.js
@@ -0,0 +1,27 @@
+
+
+var btn = document.querySelector('#btn');
+
+var nome = document.getElementById("nome_completo").value;
+var email = document.getElementById("email").value;
+var numero = document.getElementById("telefone").value;
+var endereco = document.getElementById("endereço").value;
+var senha = document.getElementById("criar_senha").value;
+
+
+btn.addEventListener("click", function (){
+console.log('dentro do event listener');
+  axios.post('http://localhost:3000/postCadastro',{
+    Nome: nome,
+    Email: email,
+    Telefone: numero,
+    Endereço: endereco,
+    Senha: senha
+  }).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+})
