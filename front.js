1786364963f72577a589582824f20466f3d01efe
Erika <erikahamakami@hotmail.com>
2021-05-08 13:08:46 -0300

    add especialidade, cidade, comercial

--- front.js
+++ front.js
@@ -5,6 +5,10 @@ var email = document.getElementById("email").value;
 var numero = document.getElementById("telefone").value;
 var endereco = document.getElementById("endereço").value;
 var senha = document.getElementById("criar_senha").value;
+var cidade = document.getElementById("cidade").value;
+var nome_comercial = document.getElementById("nome_comercial").value;
+var especialidade = document.getElementById("especialidade").value;
+var estrelas = 3;
 // adicionar comercial, especialidade e cidade
 
 btn.addEventListener("click", function (){
@@ -12,10 +16,15 @@ console.log('dentro do event listener');
   axios.post('http://localhost:3000/postCadastro',{
     // adicionar comercial, especialidade e cidade
     Nome: nome,
+    Comercial: nome_comercial,
+    Especialidade: especialidade,
     Email: email,
     Telefone: numero,
+    Cidade: cidade,
     Endereço: endereco,
-    Senha: senha
+    Senha: senha,
+    Estrelas: estrelas
+
   }).then(function (response) {
     console.log(response);
   })
