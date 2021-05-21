155a09dc3ef03ef9e2524448be8a602acd085489
Luís <luismoita1337@gmail.com>
2021-05-21 00:22:07 -0300

    ajustes em cadastro

--- cadastroCliente.js
+++ cadastroCliente.js
@@ -1,33 +1,40 @@
-var nome = document.getElementById("nome_completo").value;
-var email = document.getElementById("email").value;
-var numero = document.getElementById("telefone").value;
-var endereco = document.getElementById("endereço").value;
-var cidade = document.getElementById("cidade").value;
 
-var categoria = "Cliente"; //por padrão
 var btn = document.querySelector('#btn');
 
 
-btn.addEventListener("click", function (){
-    var senha = document.getElementById("password").value;
-    console.log('dentro do event listener');
-    console.log(senha);
 
-      axios.post('http://localhost:8888/postCadastro',{
-    
-        Nome: nome,
-        Email: email,
-        Telefone: numero,
-        Cidade: cidade,
-        Endereço: endereco,
-        Senha: senha,
-        Categoria: categoria
-    
-      }).then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    
-    })
+
+btn.addEventListener("click", async (event) =>{
+  event.preventDefault()
+
+  var nome = document.getElementById("nome_completo").value;
+  var email = document.getElementById("email").value;
+  var numero = document.getElementById("telefone").value;
+  var endereco = document.getElementById("endereço").value;
+  var cidade = document.getElementById("cidade").value;
+  var categoria = "Cliente"; //por padrão
+
+
+  var senha = document.getElementById("password").value;
+  console.log('dentro do event listener');
+  console.log(senha);
+
+  await axios.post('http://localhost:8888/postCadastro',{
+
+    Nome: nome,
+    Email: email,
+    Telefone: numero,
+    Cidade: cidade,
+    Endereço: endereco,
+    Senha: senha,
+    Categoria: categoria
+
+  }).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  location.reload()
+  window.location.href = "login.html"
+})
